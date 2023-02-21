0b55e947b29def3b79f40753527fc5ec656c3fc2
Richard Ogbuaugu <chizzy3003@gmail.com>
2023-02-21 09:15:40 -0800

    5-sign.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * main - check the code.
  *
@@ -7,11 +8,24 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower('o');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower(108);
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
