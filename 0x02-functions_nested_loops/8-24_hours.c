3319fb4433a38207b25fcbbf5b6c1a1e7330d3ee
Richard Ogbuaugu <chizzy3003@gmail.com>
2023-02-21 14:14:37 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,15 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
+	j = 0;
 
 	while (i < 24)
 	{
-		j = 0;
+		while (j < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(i + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar(j + '0');
 			_putchar('\n');
 			j++;
 		}
