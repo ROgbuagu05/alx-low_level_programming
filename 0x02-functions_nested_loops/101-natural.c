a87007b555ad8d7ec3f4c0c945142c01ad7452dd
Richard Ogbuaugu <chizzy3003@gmail.com>
2023-02-22 04:01:50 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,10 +12,8 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			sum = sum + 1;
-		}
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
 	printf("%d\n", sum);
 	return (0);
